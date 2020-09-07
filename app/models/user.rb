7e3387d5c27eec842e7d68329573a2454e152ee8
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-07 17:18:43 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: '6文字以上の半角英数字'
+    validates :firstname, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :lastname, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :firstname_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :lastname_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :password, confirmation: true
+    validates :nickname
+    validates :birthday
+  end
 end
