ee71d4fe19cf3180eb36b056d563d19c47e8ecbf
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-16 16:52:12 +0900

    多階層カテゴリーの再開

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,24 @@
-# class CategoriesController < ApplicationController
-#   before_action :set_category, only: :show
+class CategoriesController < ApplicationController
+  before_action :set_category, only: :show
 
-#   def index
-#     @parents = Category.where(ancestry: nil)
-#   end
+  def index
+    @parents = Category.where(ancestry: nil)
+  end
 
-#   def show
-#     @items = @category.set_items
-#     @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
-#   end
+  def show
+    @items = @category.set_items
+    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
+  end
   
-#   private
-#   def set_category
-#     @category = Category.find(params[:id])
-#     # 追記---------------------------------
-#     if @category.has_children?
-#       @category_links = @category.children
-#     else
-#       @category_links = @category.siblings
-#     end
-#     # -------------------------------------
-#   end
-# end
+  private
+  def set_category
+    @category = Category.find(params[:id])
+    # 追記---------------------------------
+    if @category.has_children?
+      @category_links = @category.children
+    else
+      @category_links = @category.siblings
+    end
+    # -------------------------------------
+  end
+end
