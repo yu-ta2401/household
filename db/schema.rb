8ecba59de9c7526b3abcc0baed0121a849946368
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-29 12:31:21 +0900

    ビューの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_094716) do
+ActiveRecord::Schema.define(version: 2020_09_10_110453) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_09_09_094716) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_items_on_ancestry"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
