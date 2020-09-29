9ca8916aa254ad17a8f36ad4815e19b94a4b7e64
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-09-29 12:30:07 +0900

    エラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.all.order("created_at: :desc")
+    @items = Item.all.order(created_at: :desc)
   end 
 
   def edit
