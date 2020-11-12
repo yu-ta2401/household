ecea063c02ce8f10433cf54142ed5a0a46fa2576
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-11-12 11:25:25 +0900

    管理者追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   root to: 'items#index'
   resources :articles
   resources :users, only: [:edit, :update]
