e5a4355accc0f7473c3d30901a6aa13c5b6a88a8
yu-ta2401 <sid.melodyranbu.cuoria@gmail.com>
2020-11-17 22:16:10 +0900

    管理者機能のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
-  root to: 'items#index'
+  devise_for :admins, sip: :all
+  devise_scope :adomin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'sdmin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+  
+  devise_for :admins, sip: :all
+  devise_scope :user do
+    get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in' => 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out' => 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sign_up' => 'users/registrations#new', as: 'new_user_registration'
+    post 'users' => 'users/registrations#create', as: 'user_registration'
+    get 'users/password/new' => 'users/passwords#new', as: 'new_user_password'
+  end
+
+  namespace :users do
+    resources :posts
+  end
+  root to: 'users/items#index'
   resources :articles
   resources :users, only: [:edit, :update]
   resources :items
